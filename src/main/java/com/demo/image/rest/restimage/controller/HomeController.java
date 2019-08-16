fbec43ae2c42f18a2a2bc8adda54da94ed931894
masumba <masumbakayombo1@gmail.com>
2019-08-16 17:40:10 +0200

    Removed View image method

--- src/main/java/com/demo/image/rest/restimage/controller/HomeController.java
+++ src/main/java/com/demo/image/rest/restimage/controller/HomeController.java
@@ -1,7 +1,5 @@
 package com.demo.image.rest.restimage.controller;
 
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -22,30 +20,34 @@ public class HomeController {
     @RequestMapping(value = "/upload", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<Object> uploadFile(@RequestParam("file") MultipartFile file) {
 
-        try {
-            File convertFile = new File("C:\\ADI\\" + file.getOriginalFilename());
-            convertFile.createNewFile();
-            FileOutputStream fileOutputStream = new FileOutputStream(convertFile);
-            fileOutputStream.write(file.getBytes());
-            fileOutputStream.close();
-            return new ResponseEntity<>("File is Uploaded", HttpStatus.OK);
+        if (file != null) {
+            try {
+                File convertFile = new File("C:\\ADI\\" + file.getOriginalFilename());
+                convertFile.createNewFile();
+                FileOutputStream fileOutputStream = new FileOutputStream(convertFile);
+                fileOutputStream.write(file.getBytes());
+                fileOutputStream.close();
+                return new ResponseEntity<>("File is Uploaded", HttpStatus.OK);
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            return new ResponseEntity<>("Something Went Wrong", HttpStatus.CONFLICT);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return new ResponseEntity<>("Something Went Wrong", HttpStatus.CONFLICT);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                return new ResponseEntity<>("Something Went Wrong", HttpStatus.CONFLICT);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return new ResponseEntity<>("Something Went Wrong", HttpStatus.CONFLICT);
 
+            }
         }
+
+        return new ResponseEntity<>("No Image Uploaded", HttpStatus.CONFLICT);
     }
 
     //check this
-    @RequestMapping(value = "/uploadBytes", method = RequestMethod.POST)
+    /*@RequestMapping(value = "/uploadBytes", method = RequestMethod.POST)
     public ResponseEntity<Resource> uploadBytes(@RequestParam("file") byte[] data) {
         return ResponseEntity.ok()
                 .contentType(MediaType.parseMediaType("image/jpeg"))
-                //.header(HttpHeaders.CONTENT_TYPE,"")
+                .header(HttpHeaders.CONTENT_DISPOSITION,"attachment; filename=\""+dbFile.getFileName()+"\"")
                 .body(new ByteArrayResource(data));
-    }
+    }*/
 }
